4dec0840fb853e680edf14d2cd4156d1913cb889
Mathew Newton <mathew.enewton@gmail.com>
2020-12-07 19:44:55 -0500

    Second commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,24 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 
-const KEY = "YOUR_KEY";
-
 class App extends React.Component {
-onTermSubmit = (term) => {
-    youtube.get('/search', {
+    state = { videos: [] };
+
+    onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
         params: {
             q: term,
-            part: "snippet",
-            maxResults: 5,
-            key: KEY
-        }
+        },
     });
+
+    this.setState({ videos: response.data.items });
 };
 
     render() {
         return ( 
         <div className="ui container">
             <SearchBar onFormSubmit={this.onTermSubmit} />
+            I have {this.state.videos.length} videos.
         </div>
         );
     }
